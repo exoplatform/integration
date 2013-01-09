014dfae69dfa0d9fd8380ed683fd940e2bc95df6
Tuan Pham <tuanp@exoplatform.com>
2013-01-09 17:54:47 +0700

    INTEG-124 update event activity

--- integ-calendar/integ-calendar-social/src/main/java/org/exoplatform/cs/ext/impl/CalendarSpaceActivityPublisher.java
+++ integ-calendar/integ-calendar-social/src/main/java/org/exoplatform/cs/ext/impl/CalendarSpaceActivityPublisher.java
@@ -16,10 +16,14 @@
  */
 package org.exoplatform.cs.ext.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.exoplatform.calendar.service.CalendarEvent;
+import org.exoplatform.calendar.service.CalendarService;
 import org.exoplatform.calendar.service.impl.CalendarEventListener;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.portal.application.PortalRequestContext;
@@ -28,6 +32,7 @@ import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.social.common.ExoSocialException;
+import org.exoplatform.social.core.BaseActivityProcessorPlugin;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
 import org.exoplatform.social.core.identity.model.Identity;
@@ -74,11 +79,38 @@ public class CalendarSpaceActivityPublisher extends CalendarEventListener {
   public static final String EVENT_STARTTIME_KEY   = "EventStartTime".intern();
 
   public static final String EVENT_ENDTIME_KEY     = "EventEndTime".intern();
-  
+
   public static final String EVENT_LINK_KEY        = "EventLink";
-  
+
   public static final String INVITATION_DETAIL     = "/invitation/detail/";
-  
+
+
+
+  private static final String[] SUMMARY_UPDATED = {"summary_updated", "Summary has been updated to: $value."};
+  private static final String[] DESCRIPTION_UPDATED = {"description_updated", "Description has been updated to: $value."};
+  private static final String[] FROM_UPDATED = {"fromDateTime_updated", "Start date has been updated to: $value."};
+  private static final String[] TO_UPDATED = {"toDateTime_updated", "End date has been updated to: $value."};
+  private static final String[] LOCALTE_UPDATED = {"location_updated", "Location has been updated to: $value."};
+  private static final String[] ALLDAY_UPDATED = {"allDay_updated", "Event is now an all day event."};
+  private static final String[] REPEAT_UPDATED = {"repeatType_updated", "Event will be repeated each: $value."};
+  private static final String[] ATTACH_UPDATED = {"attachment_updated", "Attachment(s) has been added to the event."};
+  private static final String[] CATEGORY_UPDATED = {"eventCategoryName_updated", "Event's category is now: $value."};
+  private static final String[] CALENDAR_UPDATED = {"calendarId_updated", "Event's calendar is now: $value."};
+  private static final String[] PRIORITY_UPDATED = {"priority_updated", "Priority is now: $value."};
+
+
+  private static final String[] NAME_UPDATED = {"name_updated", "Name has been updated to: $value."};
+  private static final String[] NOTE_UPDATED = {"note_updated", "Note has been updated to: $value."};
+  private static final String[] TASK_CATEGORY_UPDATED = {"taskCategoryName_updated", "Task's category is now: $value."};
+  private static final String[] TASK_CALENDAR_UPDATED = {"task_CalendarId_updated", "Task's calendar is now: $value."};
+  private static final String[] TASK_NEED_ACTION = {CalendarEvent.NEEDS_ACTION, "Task needs action."};
+  private static final String[] TASK_IN_PROCESS_ACTION = {CalendarEvent.IN_PROCESS, "Task is in process."};
+  private static final String[] TASK_COMPLETED_ACTION = {CalendarEvent.COMPLETED, "Task has been completed."};
+  private static final String[] TASK_CANCELLED_ACTION = {CalendarEvent.CANCELLED, "Task has been cancelled."};
+
+  private static final SimpleDateFormat dformat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+  private CalendarService calService_ ;
+
   /**
    * Make url for the event of the calendar application. 
    * Format of the url is: 
@@ -95,13 +127,13 @@ public class CalendarSpaceActivityPublisher extends CalendarEventListener {
    */
   private String makeEventLink(CalendarEvent event) {
     StringBuffer sb = new StringBuffer("");    
-      PortalRequestContext requestContext = Util.getPortalRequestContext();
-      sb.append(requestContext.getPortalURI())
-        .append(requestContext.getNodePath())
-        .append(INVITATION_DETAIL)
-        .append(ConversationState.getCurrent().getIdentity().getUserId())
-        .append("/").append(event.getId())
-        .append("/").append(event.getCalType());    
+    PortalRequestContext requestContext = Util.getPortalRequestContext();
+    sb.append(requestContext.getPortalURI())
+    .append(requestContext.getNodePath())
+    .append(INVITATION_DETAIL)
+    .append(ConversationState.getCurrent().getIdentity().getUserId())
+    .append("/").append(event.getId())
+    .append("/").append(event.getCalType());    
     return sb.toString();
   }
 
@@ -147,23 +179,226 @@ public class CalendarSpaceActivityPublisher extends CalendarEventListener {
         activity.setBody(event.getDescription());
         activity.setType(CALENDAR_APP_ID);
         activity.setTemplateParams(makeActivityParams(event, calendarId, eventType));
-
         activityM.saveActivityNoReturn(spaceIdentity, activity);
+        event.setActivityId(activity.getId());
       }
     }catch(ExoSocialException e){ //getSpaceByPrettyName
       if (LOG.isErrorEnabled())
         LOG.error("Can not record Activity for space when event added ", e);
     }
   }
-  
+
+  private void commnetToActivity(CalendarEvent event, String calendarId, String eventType) {
+
+  }
+
+  private void updateToActivity(CalendarEvent event, String calendarId, String eventType, Map<String, String[]> messagesParams){
+    try {
+      Class.forName("org.exoplatform.social.core.space.spi.SpaceService");
+    } catch (ClassNotFoundException e) {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("eXo Social components not found!", e);
+      }
+      return;
+    }
+    if (calendarId == null || calendarId.indexOf(CalendarDataInitialize.SPACE_CALENDAR_ID_SUFFIX) < 0) {
+      return;
+    }
+    try{
+      IdentityManager identityM = (IdentityManager) PortalContainer.getInstance().getComponentInstanceOfType(IdentityManager.class);
+      ActivityManager activityM = (ActivityManager) PortalContainer.getInstance().getComponentInstanceOfType(ActivityManager.class);
+      SpaceService spaceService = (SpaceService) PortalContainer.getInstance().getComponentInstanceOfType(SpaceService.class);
+      String spacePrettyName = calendarId.split(CalendarDataInitialize.SPACE_CALENDAR_ID_SUFFIX)[0];
+      Space space = spaceService.getSpaceByPrettyName(spacePrettyName);
+      if (space != null && event.getActivityId() != null) {
+        String userId = ConversationState.getCurrent().getIdentity().getUserId();
+        Identity spaceIdentity = identityM.getOrCreateIdentity(SpaceIdentityProvider.NAME, space.getPrettyName(), false);
+        Identity userIdentity = identityM.getOrCreateIdentity(OrganizationIdentityProvider.NAME, userId, false);
+        ExoSocialActivity activity = activityM.getActivity(event.getActivityId()) ;
+        if(activity == null){ activity = new ExoSocialActivityImpl() ;
+        activity.setUserId(userIdentity.getId());
+        activity.setTitle(event.getSummary());
+        activity.setBody(event.getDescription());
+        activity.setType(CALENDAR_APP_ID);
+        activity.setTemplateParams(makeActivityParams(event, calendarId, eventType));
+        activityM.saveActivityNoReturn(spaceIdentity, activity);
+        } else {
+          activity.setTitle(event.getSummary());
+          activity.setBody(event.getDescription());
+          activity.setTemplateParams(makeActivityParams(event, calendarId, eventType));
+          activityM.updateActivity(activity);
+          for(String key : messagesParams.keySet()) {
+            ExoSocialActivity newComment = new ExoSocialActivityImpl();
+            newComment.setUserId(userIdentity.getId());
+            newComment.isComment(true);
+            newComment.setType("cs-calendar:spaces");
+            newComment.setTitleId(key);
+            Map<String, String> data = new HashMap<String, String>(); 
+            data.put(key, messagesParams.get(key)[1]);
+            data.put(BaseActivityProcessorPlugin.TEMPLATE_PARAM_TO_PROCESS, key);
+            newComment.setTemplateParams(data);
+            newComment.setTitle(messagesParams.get(key)[0]);
+            activityM.saveComment(activity, newComment);
+          } 
+        }
+      }
+
+    } catch (ExoSocialException e){  
+      if (LOG.isErrorEnabled())
+        LOG.error("Can not update Activity for space when event modified ", e);
+    }
+  }
+
+  private void deleteActivity(CalendarEvent event, String calendarId, String eventType){
+    try {
+      Class.forName("org.exoplatform.social.core.space.spi.SpaceService");
+    } catch (ClassNotFoundException e) {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("eXo Social components not found!", e);
+      }
+      return;
+    }
+    if (calendarId == null || calendarId.indexOf(CalendarDataInitialize.SPACE_CALENDAR_ID_SUFFIX) < 0) {
+      return;
+    }
+    try{
+      ActivityManager activityM = (ActivityManager) PortalContainer.getInstance().getComponentInstanceOfType(ActivityManager.class);
+      SpaceService spaceService = (SpaceService) PortalContainer.getInstance().getComponentInstanceOfType(SpaceService.class);
+      String spacePrettyName = calendarId.split(CalendarDataInitialize.SPACE_CALENDAR_ID_SUFFIX)[0];
+      Space space = spaceService.getSpaceByPrettyName(spacePrettyName);
+      if (space != null && event.getActivityId() != null) {
+        activityM.deleteActivity(event.getActivityId());
+      }
+    } catch (ExoSocialException e){ //getSpaceByPrettyName
+      if (LOG.isErrorEnabled())
+        LOG.error("Can not delete Activity for space when event deleted ", e);
+    }
+  }
+  private Map<String, String[]> buildParams(CalendarEvent oldEvent, CalendarEvent newEvent){
+    Map<String, String[]> messagesParams = new HashMap<String, String[]>();
+    if(CalendarEvent.TYPE_EVENT.equals(newEvent.getEventType())) {
+      if(!oldEvent.getSummary().equals(newEvent.getSummary())) {
+        messagesParams.put(SUMMARY_UPDATED[0],new String[]{SUMMARY_UPDATED[1].replace("$value", newEvent.getSummary()),newEvent.getSummary()}) ;
+      }
+    }/*
+      if(oldEvent.getDescription() != null && !oldEvent.getDescription().equals(newEvent.getDescription())) {
+        messagesParams.put(DESCRIPTION_UPDATED[0],new String[]{DESCRIPTION_UPDATED[1].replace("$value", newEvent.getDescription()),newEvent.getDescription()}) ;
+      }
+      if(!isAllDayEvent(oldEvent) || !isAllDayEvent(oldEvent)) {
+        if(!oldEvent.getAttachment().equals(newEvent.getAttachment())) {
+          messagesParams.put(ALLDAY_UPDATED[0],new String[]{ALLDAY_UPDATED[1], ALLDAY_UPDATED[1]}) ;
+        }
+      } else {
+        if(!oldEvent.getFromDateTime().equals(newEvent.getFromDateTime())) {
+          messagesParams.put(FROM_UPDATED[0],new String[]{FROM_UPDATED[1].replace("$value", dformat.format(newEvent.getFromDateTime())),dformat.format(newEvent.getFromDateTime())}) ;
+        }
+        if(!oldEvent.getToDateTime().equals(newEvent.getToDateTime())) {
+          messagesParams.put(TO_UPDATED[0],new String[]{TO_UPDATED[1].replace("$value", dformat.format(newEvent.getToDateTime())),dformat.format(newEvent.getToDateTime())}) ;
+        }
+      }
+      if(oldEvent.getLocation()!= null && !oldEvent.getLocation().equals(newEvent.getLocation())) {
+        messagesParams.put(LOCALTE_UPDATED[0],new String[]{LOCALTE_UPDATED[1].replace("$value", newEvent.getLocation()),newEvent.getLocation()}) ;
+      }
+      if(oldEvent.getRepeatType()!= null && !oldEvent.getRepeatType().equals(newEvent.getRepeatType())) {
+        messagesParams.put(REPEAT_UPDATED[0],new String[]{REPEAT_UPDATED[1].replace("$value", newEvent.getRepeatType()),newEvent.getRepeatType()}) ;
+      }
+      if(oldEvent.getAttachment() != null &&  !oldEvent.getAttachment().equals(newEvent.getAttachment())) {
+        messagesParams.put(ATTACH_UPDATED[0],new String[]{ATTACH_UPDATED[1], ATTACH_UPDATED[1]}) ;
+      }
+      if(!oldEvent.getEventCategoryName().equals(newEvent.getEventCategoryName())) {
+        messagesParams.put(CATEGORY_UPDATED[0],new String[]{CATEGORY_UPDATED[1].replace("$value", newEvent.getEventCategoryName()),newEvent.getEventCategoryName()}) ;
+      }
+      if(!oldEvent.getCalendarId().equals(newEvent.getCalendarId())) {
+        try {
+          CalendarService calService = (CalendarService) PortalContainer.getInstance().getComponentInstanceOfType(CalendarService.class);
+          org.exoplatform.calendar.service.Calendar cal = calService.getGroupCalendar(newEvent.getCalendarId());
+          messagesParams.put(CALENDAR_UPDATED[0],new String[]{CALENDAR_UPDATED[1].replace("$value", cal.getName()),cal.getName()}) ;
+        } catch (Exception e) {
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Calendar is not found!", e);
+          }
+        }
+      }
+      if(oldEvent.getPriority()!= null && !oldEvent.getPriority().equals(newEvent.getPriority())) {
+        messagesParams.put(PRIORITY_UPDATED[0],new String[]{PRIORITY_UPDATED[1].replace("$value", newEvent.getPriority()),newEvent.getPriority()}) ;
+      }
+    } else {
+      if(!oldEvent.getSummary().equals(newEvent.getSummary())) {
+        messagesParams.put(NAME_UPDATED[0],new String[]{NAME_UPDATED[1].replace("$value", newEvent.getSummary()),newEvent.getSummary()}) ;
+      }
+      if(!oldEvent.getDescription().equals(newEvent.getDescription())) {
+        messagesParams.put(NOTE_UPDATED[0],new String[]{NOTE_UPDATED[1].replace("$value", newEvent.getDescription()),newEvent.getDescription()}) ;
+      }
+      if(!oldEvent.getFromDateTime().equals(newEvent.getFromDateTime())) {
+        messagesParams.put(FROM_UPDATED[0],new String[]{FROM_UPDATED[1].replace("$value", dformat.format(newEvent.getFromDateTime())),dformat.format(newEvent.getFromDateTime())}) ;
+      }
+      if(!oldEvent.getToDateTime().equals(newEvent.getToDateTime())) {
+        messagesParams.put(TO_UPDATED[0],new String[]{TO_UPDATED[1].replace("$value", dformat.format(newEvent.getToDateTime())),dformat.format(newEvent.getToDateTime())}) ;
+      }
+      if(oldEvent.getAttachment() != null && !oldEvent.getAttachment().equals(newEvent.getAttachment())) {
+        messagesParams.put(ATTACH_UPDATED[0],new String[]{ATTACH_UPDATED[1], ATTACH_UPDATED[1]}) ;
+      }
+      if(!oldEvent.getEventCategoryName().equals(newEvent.getEventCategoryName())) {
+        messagesParams.put(TASK_CATEGORY_UPDATED[0],new String[]{TASK_CATEGORY_UPDATED[1].replace("$value", newEvent.getEventCategoryName()),newEvent.getEventCategoryName()}) ;
+      }
+      if(!oldEvent.getCalendarId().equals(newEvent.getCalendarId())) {
+        try {
+          CalendarService calService = (CalendarService) PortalContainer.getInstance().getComponentInstanceOfType(CalendarService.class);
+          org.exoplatform.calendar.service.Calendar cal = calService.getGroupCalendar(newEvent.getCalendarId());
+          messagesParams.put(TASK_CALENDAR_UPDATED[0],new String[]{TASK_CALENDAR_UPDATED[1].replace("$value", cal.getName()),cal.getName()}) ;
+        } catch (Exception e) {
+          if (LOG.isDebugEnabled()) {
+            LOG.debug("Calendar is not found!", e);
+          }
+        }
+      }
+      if(!oldEvent.getStatus().equals(newEvent.getStatus())) {
+        if(CalendarEvent.NEEDS_ACTION.equals(newEvent.getStatus())) {
+          messagesParams.put(TASK_NEED_ACTION[0],new String[]{TASK_NEED_ACTION[1].replace("$value", newEvent.getStatus()),newEvent.getStatus()}) ;
+        } else if(CalendarEvent.IN_PROCESS.equals(newEvent.getStatus())) {
+          messagesParams.put(TASK_IN_PROCESS_ACTION[0],new String[]{TASK_IN_PROCESS_ACTION[1].replace("$value", newEvent.getStatus()),newEvent.getStatus()}) ;
+        } else if(CalendarEvent.COMPLETED.equals(newEvent.getStatus())) {
+          messagesParams.put(TASK_COMPLETED_ACTION[0],new String[]{TASK_COMPLETED_ACTION[1].replace("$value", newEvent.getStatus()),newEvent.getStatus()}) ;
+        } else if(CalendarEvent.CANCELLED.equals(newEvent.getStatus())) {
+          messagesParams.put(TASK_CANCELLED_ACTION[0],new String[]{TASK_CANCELLED_ACTION[1].replace("$value", newEvent.getStatus()),newEvent.getStatus()}) ;
+        }
+      }
+    }*/
+    
+    return messagesParams;
+  }
+
+  private boolean isAllDayEvent(CalendarEvent eventCalendar) {
+    Calendar calendar = GregorianCalendar.getInstance() ;
+    calendar.setLenient(false);
+    Calendar cal1 = calendar ;
+    Calendar cal2 = calendar ;
+    cal1.setTime(eventCalendar.getFromDateTime()) ;
+    cal2.setTime(eventCalendar.getToDateTime()) ;
+    return (cal1.get(Calendar.HOUR_OF_DAY) == 0  && 
+        cal1.get(Calendar.MINUTE) == 0 &&
+        cal2.get(Calendar.HOUR_OF_DAY) == cal2.getActualMaximum(Calendar.HOUR_OF_DAY)&& 
+        cal2.get(Calendar.MINUTE) == cal2.getActualMaximum(Calendar.MINUTE) );
+  }
+
   public void savePublicEvent(CalendarEvent event, String calendarId) {
     String eventType = event.getEventType().equalsIgnoreCase(CalendarEvent.TYPE_EVENT) ? EVENT_ADDED : TASK_ADDED;
     publishActivity(event, calendarId, eventType);
   }
 
-  public void updatePublicEvent(CalendarEvent event, String calendarId) {
-    String eventType = event.getEventType().equalsIgnoreCase(CalendarEvent.TYPE_EVENT) ? EVENT_UPDATED : TASK_UPDATED;
-    publishActivity(event, calendarId, eventType);
+  public void updatePublicEvent(CalendarEvent event, String calendarId) {}
+
+
+  public void updatePublicEvent(CalendarEvent oldEvent, CalendarEvent newEvent, String calendarId) {
+    String eventType = newEvent.getEventType().equalsIgnoreCase(CalendarEvent.TYPE_EVENT) ? EVENT_ADDED : TASK_ADDED;
+    Map<String, String[]> messagesParams = buildParams(oldEvent, newEvent);
+    updateToActivity(newEvent, calendarId, eventType, messagesParams);
   }
+
+  public void deletePublicEvent(CalendarEvent event, String calendarId) {
+    String eventType = event.getEventType().equalsIgnoreCase(CalendarEvent.TYPE_EVENT) ? EVENT_ADDED : TASK_ADDED;
+    deleteActivity(event, calendarId, eventType) ;
+  }
+
 
 }
