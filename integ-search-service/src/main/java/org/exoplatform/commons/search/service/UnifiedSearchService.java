9ff1919a2c50ac62bef45ff4304891c949006585
Canh Pham Van <canhpv@exoplatform.com>
2013-04-09 15:02:12 +0700

    INTEG-154: PLF 4 - Unified Search - REST APIs doc and generation

--- integ-search-service/src/main/java/org/exoplatform/commons/search/service/UnifiedSearchService.java
+++ integ-search-service/src/main/java/org/exoplatform/commons/search/service/UnifiedSearchService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2003-2012 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.exoplatform.commons.search.service;
 
 import java.io.File;
@@ -44,6 +60,15 @@ import org.exoplatform.web.controller.metadata.ControllerDescriptor;
 import org.exoplatform.web.controller.metadata.DescriptorBuilder;
 import org.exoplatform.web.controller.router.Router;
 
+/**
+ * Created by The eXo Platform SAS
+ * Author : Tung Vu Minh
+ *          tungvm@exoplatform.com
+ * Nov 21, 2012  
+ * This class provides end points help all external components call unified search service by Restful service standard.
+ * These services are search, search setting, quick search setting, registry, sites and enable search type
+ * @LevelAPI Experimental
+ */
 @Path("/search")
 @Produces(MediaType.APPLICATION_JSON)
 public class UnifiedSearchService implements ResourceContainer {
@@ -66,6 +91,14 @@ public class UnifiedSearchService implements ResourceContainer {
   private SettingService settingService;
   private Router router;
   
+  /**
+   * A constructor creates a instance of unified search service with the specified parameters
+   * @param searchService a service to work with other connectors
+   * @param settingService a service to store and get the setting values 
+   * @param userPortalConfigService a service to get user information from portal
+   * @param webAppController a controller to get configuration path
+   * @LevelAPI Experimental
+   */
   public UnifiedSearchService(SearchService searchService, SettingService settingService, UserPortalConfigService userPortalConfigService, WebAppController webAppController){
     this.searchService = searchService;
     this.settingService = settingService;
@@ -190,7 +223,7 @@ public class UnifiedSearchService implements ResourceContainer {
     }
   }
 
-  
+    
   @SuppressWarnings("unchecked")
   private SearchSetting getSearchSetting() {
     try {
