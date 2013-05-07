e97b91671c3404f54e92935614c22880d64bc466
Canh Pham Van <canhpv@exoplatform.com>
2013-05-07 11:14:00 +0700

    PLF-4824: Download the file from the SearchResult

--- integ-search-portlet/src/main/java/org/exoplatform/commons/quicksearch/assets/quicksearch.js
+++ integ-search-portlet/src/main/java/org/exoplatform/commons/quicksearch/assets/quicksearch.js
@@ -226,6 +226,10 @@ function initQuickSearch(portletId,seeAllMsg, noResultMsg, searching) {
           break;
 
         case "file":
+        	var cssClasses = $.map(result.fileType.split(/\s+/g), function(type){return "uiIcon24x24" + type}).join(" ");
+        	avatar = CSS_AVATAR_TEMPLATE.replace(/%{cssClass}/g, cssClasses);
+        	avatar = "<a href='"+result.url+"'>" + avatar + "</a>";
+        	break;
         case "document":
         case "page":
           var cssClasses = $.map(result.fileType.split(/\s+/g), function(type){return "uiIcon24x24" + type}).join(" ");
--- integ-search-portlet/src/main/java/org/exoplatform/commons/unifiedsearch/assets/search.js
+++ integ-search-portlet/src/main/java/org/exoplatform/commons/unifiedsearch/assets/search.js
@@ -187,6 +187,10 @@ function initSearch() {
           break;
 
         case "file":
+            var cssClasses = $.map(result.fileType.split(/\s+/g), function(type){return "uiIcon64x64" + type}).join(" ");
+            avatar = CSS_AVATAR_TEMPLATE.replace(/%{cssClass}/g, cssClasses);
+            avatar = "<a href='"+result.url+"'>" + avatar + "</a>";            
+            break;        	        	
         case "document":
         case "page":
           var cssClasses = $.map(result.fileType.split(/\s+/g), function(type){return "uiIcon64x64" + type}).join(" ");
