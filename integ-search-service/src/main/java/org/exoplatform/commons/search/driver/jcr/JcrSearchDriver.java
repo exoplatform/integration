752352afd01219e32354a8b5039da5df9a46af48
Canh Pham Van <canhpv@exoplatform.com>
2013-11-15 12:19:08 +0700

    INTEG-249: [Unified Search] Externalize the fuzzy search setting
    Fix description:
    * Remove the hard-coded fuzzy search operator
    * Allow administrators to activate/deactivate this option and tune similarity value
    * Validate the similarity value.
    * Handle the tilde character in user query in both cases of fuzzy setting.

--- integ-search-service/src/main/java/org/exoplatform/commons/search/driver/jcr/JcrSearchDriver.java
+++ integ-search-service/src/main/java/org/exoplatform/commons/search/driver/jcr/JcrSearchDriver.java
@@ -1,5 +1,13 @@
 package org.exoplatform.commons.search.driver.jcr;
 
+import org.exoplatform.commons.api.search.SearchService;
+import org.exoplatform.commons.api.search.SearchServiceConnector;
+import org.exoplatform.commons.api.search.data.SearchContext;
+import org.exoplatform.commons.api.search.data.SearchResult;
+import org.exoplatform.commons.search.service.UnifiedSearchService;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -9,21 +17,14 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.exoplatform.commons.api.search.SearchService;
-import org.exoplatform.commons.api.search.SearchServiceConnector;
-import org.exoplatform.commons.api.search.data.SearchContext;
-import org.exoplatform.commons.api.search.data.SearchResult;
-import org.exoplatform.commons.search.service.UnifiedSearchService;
-import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.log.Log;
-
 public class JcrSearchDriver extends SearchService {
   private final static Log LOG = ExoLogger.getLogger(JcrSearchDriver.class);
   
   @Override
   public Map<String, Collection<SearchResult>> search(SearchContext context, String query, Collection<String> sites, Collection<String> types, int offset, int limit, String sort, String order) {
+    String fuzzySyntax = getFuzzySyntax();
     HashMap<String, ArrayList<String>> terms = parse(query); //parse query for single and quoted terms
-    query = repeat("\"%s\"", terms.get("quoted"), " ") + " " + repeat("%s~", terms.get("single"), " "); //add a ~ after each single term (for fuzzy search)
+    query = repeat("\"%s\"", terms.get("quoted"), " ") + " " + repeat("%s" + fuzzySyntax, terms.get("single"), " "); //add a fuzzySyntax after each single term (for fuzzy search)
 
     Map<String, Collection<SearchResult>> results = new HashMap<String, Collection<SearchResult>>();
     if(null==types || types.isEmpty()) return results;
@@ -66,10 +67,61 @@ public class JcrSearchDriver extends SearchService {
     StringBuilder sb=new StringBuilder();
     String delim = "";
     for(String str:strArr) {
+      if (!isEnableFuzzySearch()){
+        String disableFuzzy = str.replace("~", "");
+        sb.append(delim).append(disableFuzzy);
+        delim = delimiter;        
+      } else if (!isFuzzyManual(str) && str.indexOf("~") != -1) {
+        str = str.replace(str.substring(str.indexOf("~")), "~0.5");
+        sb.append(delim).append(str);
+        delim = delimiter;        
+      }else if (!isFuzzyManual(str) && isEnableFuzzySearch()){
       sb.append(delim).append(String.format(format, str));
       delim = delimiter;
+      }else {
+        sb.append(delim).append(str);
+        delim = delimiter;
+      }
     }
     return sb.toString();
+  }
+
+  private static String getFuzzySyntax() {
+    String fuzzySyntax = "";
+    String fuzzySimilarity = System.getProperty("unified-search.engine.fuzzy.similarity");
+	  Double fuzzySimilarityDouble = 0.5;
+    if (isEnableFuzzySearch()){
+	    if (fuzzySimilarity != null) {
+		    try {
+			    fuzzySimilarityDouble = Double.parseDouble(fuzzySimilarity);
+		    } catch (NumberFormatException e) {
+			    fuzzySimilarityDouble = 0.5;
+		    }
+	    }
+	    if (fuzzySimilarityDouble < 0 || fuzzySimilarityDouble >= 1) {
+		    fuzzySimilarityDouble = 0.5;
+	    }
+      fuzzySyntax = "~" + String.valueOf(fuzzySimilarityDouble);
+    }
+    return fuzzySyntax;
+  }  
+  
+  private static boolean isFuzzyManual(String input) {
+    Matcher matcher = Pattern.compile(".[~][0]([\\.][0-9])").matcher(input);
+    while (matcher.find()){
+      return true;
+    }    
+    return false;
+  }
+  
+  private static boolean isEnableFuzzySearch(){
+    String fuzzyEnable = System.getProperty("unified-search.engine.fuzzy.enable");
+
+    if ((fuzzyEnable!=null && Boolean.parseBoolean(fuzzyEnable)==true)
+        || fuzzyEnable==null)
+      return true;
+    
+    return false;
   }
 
 }
