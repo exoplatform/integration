2926b5ce4d47f176497d7b45a5867996bbeb2926
trungjc <trungnguyenthanh70@gmail.com>
2014-02-11 17:00:50 +0700

    UI-2815: update anwsers icon

--- integ-search-portlet/src/main/webapp/js/common/quicksearch.js
+++ integ-search-portlet/src/main/webapp/js/common/quicksearch.js
@@ -261,7 +261,7 @@ window.initQuickSearch = function initQuickSearch(portletId,seeAllMsg, noResultM
           break;
 
         case "answer":
-        	line = LINE_RESULT_TEMPLATE.replace(/%{cssClass}/g, "uiIconSocAnswersMini uiIconSocLightGray");	      
+        	line = LINE_RESULT_TEMPLATE.replace(/%{cssClass}/g, "uiIconPLFAnswers uiIconPLFLightGray");	      
           break;
         case "wiki":        	
         	line = LINE_RESULT_TEMPLATE.replace(/%{cssClass}/g, "uiIconWikiWiki uiIconWikiLightGray");	      
