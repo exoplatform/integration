bf1d890c9b15273ce22b199740adc3b40ec672cc
nguyenthienhuong <nguyenthienhuong0305@gmail.com>
2014-04-23 15:46:50 +0700

    INTEG-289: [Security] Detail of question in FORUM has XSS vulnerability
    Fix description:
    * Revert the previous commit (c0b9f0437) which cleans the whole scripts inside activity content
    * Use activity body (XSS vulnerability has been handled) for activity content.

--- integ-forum/integ-forum-social/src/main/java/org/exoplatform/forum/ext/impl/ActivityUtils.java
+++ integ-forum/integ-forum-social/src/main/java/org/exoplatform/forum/ext/impl/ActivityUtils.java
@@ -32,7 +32,7 @@ public class ActivityUtils {
   }
   
   public static String processContent(String content) {
-    content = CommonUtils.processBBCode(CommonUtils.decodeSpecialCharToHTMLnumber(content));
+    content = CommonUtils.processBBCode(CommonUtils.decodeSpecialCharToHTMLnumberIgnore(content));
     content = ForumActivityBuilder.getFourFirstLines(content);
     return content;
   }
