6cc85367b935354d64535df89f404327ef96fb97
Pham Duy Dong <dongpd@exoplatform.com>
2014-12-02 17:15:45 +0700

    Add function isWebContent for UIDocumentPreview.java

--- integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/ext/component/activity/UIDocumentPreview.java
+++ integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/ext/component/activity/UIDocumentPreview.java
@@ -1,17 +1,32 @@
 package org.exoplatform.wcm.ext.component.activity;
 
 
+import org.exoplatform.social.plugin.doc.UIDocViewer;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIPopupWindow;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 
+import javax.jcr.Node;
+
 @ComponentConfig(
         template = "war:/groovy/ecm/social-integration/UIDocumentPreview.gtmpl",
         events = {@EventConfig(listeners = {UIDocumentPreview.CloseActionListener.class}, name = "ClosePopup")}
 )
 public class UIDocumentPreview extends UIPopupWindow {
+
+  public boolean isWebContent() throws Exception {
+    UIDocViewer uiDocViewer = findFirstComponentOfType(UIDocViewer.class);
+    if (uiDocViewer != null) {
+      Node previewNode = uiDocViewer.getNode();
+      if (previewNode != null) {
+        return previewNode.isNodeType(org.exoplatform.ecm.webui.utils.Utils.EXO_WEBCONTENT);
+      }
+    }
+
+    return false;
+  }
 
   public static class CloseActionListener extends EventListener<UIDocumentPreview> {
     public void execute(Event<UIDocumentPreview> event) throws Exception {
