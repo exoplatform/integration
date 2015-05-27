9f95aec0b8605f4108333e4ac2d8b8e54e606219
rtouzi <rtouzi@exoplatform.com>
2015-05-27 18:04:46 +0100

    INTEG-196: [Unified Search] i18n of some labels in Floating result, Result page, Administration page
    Fix description:
    * Use eXo.ecm.WCMUtils.getBundle() to get i18n labels in JavaScript files.
    
    Conflicts:
    	integ-search-service/src/main/resources/conf/portal/configuration.xml

--- integ-search-portlet/src/main/webapp/js/common/searchAdmin.js
+++ integ-search-portlet/src/main/webapp/js/common/searchAdmin.js
@@ -7,7 +7,7 @@ function initSearchAdmin() {
           <td>%{displayName}</td> \
           <td>%{description}</td> \
           <td class='center'> \
-            <input type='button' class='btn btn-mini contentType' id='%{id}' value='Enable'> \
+            <input type='button' class='btn btn-mini contentType' id='%{id}' name='Enable' value='%{key}'> \
           </td> \
         </tr> \
       ";
@@ -15,30 +15,34 @@ function initSearchAdmin() {
       var connectors = registry[0];
       var searchTypes = registry[1];
       $.each(connectors, function(searchType, connector){
-        $("#searchAdmin table").append(row_template.replace(/%{id}/g, connector.searchType)
-                                                  .replace(/%{displayName}/g, connector.displayName)
-                                                  .replace(/%{description}/g, connector.description));
+        $("#searchAdmin table").append(row_template.replace(/%{id}/g, connector.searchType).replace(/%{key}/g,eXo.ecm.WCMUtils.getBundle('SearchAdmin.action.Enable', eXo.env.portal.language))
+                                                  .replace(/%{displayName}/g, eXo.ecm.WCMUtils.getBundle("SearchAdmin.type." + connector.displayName , eXo.env.portal.language))
+                                                  .replace(/%{description}/g,  eXo.ecm.WCMUtils.getBundle("SearchAdmin.type." + connector.displayName + ".description", eXo.env.portal.language)));
       });
 
       $.each(searchTypes, function(i, type){
-        $(".contentType#"+type).val("Disable");
+        $(".contentType#"+type).val(eXo.ecm.WCMUtils.getBundle("SearchAdmin.action.Disable", eXo.env.portal.language));
+        $(".contentType#"+type).attr("name","Disable");
+
         //$(".ContentType#"+type).next().attr("disabled", false);
       });
     });
 
 
     $(".contentType").live("click", function(){
-      if("Enable"==$(this).val()) {
-        $(this).val("Disable");
+      if("Enable"==$(this).attr("name")) {
+        $(this).attr("name","Disable");
+        $(this).val(eXo.ecm.WCMUtils.getBundle("SearchAdmin.action.Disable", eXo.env.portal.language));
         //$(this).next().attr("disabled", false);
       } else {
-        $(this).val("Enable");
+        $(this).attr("name","Enable");
+        $(this).val(eXo.ecm.WCMUtils.getBundle("SearchAdmin.action.Enable", eXo.env.portal.language));
         //$(this).next().attr("disabled", true);
       }
 
       var enabledTypes = [];
       $.each($(".contentType"), function(){
-        if("Disable"==this.value) enabledTypes.push(this.id);
+        if("Disable"== this.name) enabledTypes.push(this.id);
       });
       
       var jqxhr = $.post("/rest/search/enabled-searchtypes/"+enabledTypes, {
