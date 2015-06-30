559fed73adb08d119862c9aaba409df68e96d182
binhdv <binhdv@exoplatform.com>
2015-06-30 15:42:17 +0700

    SOC-4950 | Implement update listener for topic and poll activity

--- integ-forum/integ-forum-social/src/main/java/org/exoplatform/forum/addons/rdbms/listener/ForumActivityUpdaterListener.java
+++ integ-forum/integ-forum-social/src/main/java/org/exoplatform/forum/addons/rdbms/listener/ForumActivityUpdaterListener.java
@@ -1,6 +1,9 @@
 package org.exoplatform.forum.addons.rdbms.listener;
 
 import org.exoplatform.forum.ext.activity.ForumActivityBuilder;
+import org.exoplatform.forum.ext.activity.ForumActivityUtils;
+import org.exoplatform.forum.service.Post;
+import org.exoplatform.forum.service.Topic;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.log.ExoLogger;
@@ -13,12 +16,18 @@ public class ForumActivityUpdaterListener extends Listener<ExoSocialActivity, St
   @Override
   public void onEvent(Event<ExoSocialActivity, String> event) throws Exception {
     ExoSocialActivity activity = event.getSource();
+    String newActivityId = event.getData();
     if (ForumActivityBuilder.FORUM_ACTIVITY_TYPE.equals(activity.getType())) {
       if (activity.isComment()) {
-        LOG.info(String.format("Migration the forum comment '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), event.getData()));
+        Post post = ForumActivityUtils.getPost(activity);
+        ForumActivityUtils.takeCommentBack(post, newActivityId);
+        LOG.info(String.format("Migration the forum comment '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), newActivityId));
       } else {
-        LOG.info(String.format("Migration the forum activity '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), event.getData()));
+        Topic topic = ForumActivityUtils.getTopic(activity);
+        ForumActivityUtils.takeActivityBack(topic, newActivityId);
+        LOG.info(String.format("Migration the forum activity '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), newActivityId));
       }
     }
   }
+  
 }
