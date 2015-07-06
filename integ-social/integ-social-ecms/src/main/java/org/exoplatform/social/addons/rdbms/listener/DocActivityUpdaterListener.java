eaa9686d72af343d202bf0b53730dd19dfa0f11d
thanhvc <thanhvc@exoplatform.com>
2015-07-06 13:41:03 +0700

    SOC-4954 | [Social Activity Addon] Implements the Listener on INTEG to update the activityId

--- integ-social/integ-social-ecms/src/main/java/org/exoplatform/social/addons/rdbms/listener/DocActivityUpdaterListener.java
+++ integ-social/integ-social-ecms/src/main/java/org/exoplatform/social/addons/rdbms/listener/DocActivityUpdaterListener.java
@@ -2,11 +2,18 @@ package org.exoplatform.social.addons.rdbms.listener;
 
 import static org.exoplatform.social.plugin.doc.UIDocActivityBuilder.ACTIVITY_TYPE;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.exoplatform.commons.utils.ActivityTypeUtils;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.wcm.core.NodeLocation;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.plugin.doc.UIDocActivity;
+import org.exoplatform.social.plugin.doc.UIDocActivityComposer;
 
 public class DocActivityUpdaterListener extends Listener<ExoSocialActivity, String> {
   private static final Log LOG = ExoLogger.getLogger(DocActivityUpdaterListener.class);
@@ -15,11 +22,58 @@ public class DocActivityUpdaterListener extends Listener<ExoSocialActivity, Stri
   public void onEvent(Event<ExoSocialActivity, String> event) throws Exception {
     ExoSocialActivity activity = event.getSource();
     if (ACTIVITY_TYPE.equals(activity.getType())) {
+      String newId = event.getData();
+      
       if (activity.isComment()) {
-        LOG.info(String.format("Migration %s doc comment '%s' with old id's %s and new id's %s", activity.getType(), activity.getTitle(), activity.getId(), event.getData()));
+        LOG.info(String.format("Migration doc comment '%s' with new id's %s", activity.getTitle(), newId));
+        String docCommentId = activity.getTemplateParams().get(UIDocActivity.ID);
+        Node docCommentNode = getDocNode(UIDocActivityComposer.REPOSITORY, UIDocActivityComposer.WORKSPACE, docCommentId);
+        //TODO: Needs to confirm with ECMS team about the comment type Utils.CONTENT_SPACES = "contents:spaces"
+        // Asks ECMS team to update the comment 
+        // There is new mixin type define to keep the CommentId
+        //private static String MIX_COMMENT                = "exo:activityComment";
+        //private static String MIX_COMMENT_ID             = "exo:activityCommentID";
+        if (docCommentNode.isNodeType(ActivityTypeUtils.EXO_ACTIVITY_INFO)) {
+          try {
+            docCommentNode.setProperty(ActivityTypeUtils.EXO_ACTIVITY_ID, newId);
+            docCommentNode.save();
+          } catch (RepositoryException e) {
+            LOG.warn("Updates the document activity is unsuccessful!");
+            LOG.debug("Updates the document activity is unsuccessful!", e);
+          }
+        }
+        
       } else {
-        LOG.info(String.format("Migration %s doc activity '%s' with old id's %s and new id's %s", activity.getType(), activity.getTitle(), activity.getId(), event.getData()));
+        LOG.info(String.format("Migration doc activity '%s' with new id's %s", activity.getTitle(), newId));
+        
+        String docId = activity.getTemplateParams().get(UIDocActivity.ID);
+        if (docId == null) return;
+        
+        Node docNode = getDocNode(UIDocActivityComposer.REPOSITORY, UIDocActivityComposer.WORKSPACE, docId);
+        if (docNode.isNodeType(ActivityTypeUtils.EXO_ACTIVITY_INFO)) {
+          try {
+            docNode.setProperty(ActivityTypeUtils.EXO_ACTIVITY_ID, newId);
+            docNode.save();
+          } catch (RepositoryException e) {
+            LOG.warn("Updates the document activity is unsuccessful!");
+            LOG.debug("Updates the document activity is unsuccessful!", e);
+          }
+        }
       }
     }
+  }
+  
+  /**
+   * This method is target to get the Document node following 
+   * the way in UIDocActivityComposer.getDocNode().
+   * 
+   * @param repository
+   * @param workspace
+   * @param nodeId
+   * @return
+   */
+  private Node getDocNode(String repository, String workspace, String nodeId) {
+    NodeLocation nodeLocation = new NodeLocation(repository, workspace, null, nodeId);
+    return NodeLocation.getNodeByLocation(nodeLocation);
   }
 }
--- integ-wiki/integ-wiki-social/src/main/java/org/exoplatform/wiki/addons/rdbms/listener/WikiActivityUpdaterListener.java
+++ integ-wiki/integ-wiki-social/src/main/java/org/exoplatform/wiki/addons/rdbms/listener/WikiActivityUpdaterListener.java
@@ -1,32 +1,57 @@
 package org.exoplatform.wiki.addons.rdbms.listener;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.exoplatform.commons.utils.ActivityTypeUtils;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.wiki.ext.impl.WikiSpaceActivityPublisher;
+import org.exoplatform.wiki.mow.api.Page;
 import org.exoplatform.wiki.service.WikiService;
 
 public class WikiActivityUpdaterListener extends Listener<ExoSocialActivity, String> {
   private static final Log LOG = ExoLogger.getLogger(WikiActivityUpdaterListener.class);
+  private final WikiService service;
 
   /**
-   * Constructor
+   * Do not remove wiki service on constructor, puts the service to 
+   * the constructor to make sure the service already created before using.
+   * 
    * @param wikiService 
-   * Do not remove wiki service on constructor, it use for order Startable of migration.
+   * 
    */
   public WikiActivityUpdaterListener(WikiService wikiService) {
+    this.service = wikiService;
   }
 
   @Override
   public void onEvent(Event<ExoSocialActivity, String> event) throws Exception {
     ExoSocialActivity activity = event.getSource();
     if (WikiSpaceActivityPublisher.PAGE_ID_KEY.equals(activity.getType())) {
-      if (activity.isComment()) {
-        LOG.info(String.format("Migration the wiki comment '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), event.getData()));
-      } else {
-        LOG.info(String.format("Migration the wiki activity '%s' with old id's %s and new id's %s", activity.getTitle(), activity.getId(), event.getData()));
+      String newActivityId = event.getData();
+      if (!activity.isComment()) {
+        LOG.info(String.format("Migration the wiki activity '%s' with new id:: %s", activity.getTitle(), newActivityId));
+        String pageId = activity.getTemplateParams().get(WikiSpaceActivityPublisher.PAGE_ID_KEY);
+        if (pageId == null) return;
+        String pageType = activity.getTemplateParams().get(WikiSpaceActivityPublisher.PAGE_TYPE_KEY);
+        String pageOwner = activity.getTemplateParams().get(WikiSpaceActivityPublisher.PAGE_OWNER_KEY);
+        
+        Page page = service.getPageById(pageType, pageOwner, pageId);
+        Node node = page.getJCRPageNode();
+        if (node.isNodeType(ActivityTypeUtils.EXO_ACTIVITY_INFO)) {
+          try {
+            node.setProperty(ActivityTypeUtils.EXO_ACTIVITY_ID, newActivityId);
+            node.save();
+          } catch (RepositoryException e) {
+            LOG.warn("Updates the wiki activity is unsuccessful!");
+            LOG.debug("Updates the wiki activity is unsuccessful!", e);
+          }
+        }
+        
       }
     }
   }
