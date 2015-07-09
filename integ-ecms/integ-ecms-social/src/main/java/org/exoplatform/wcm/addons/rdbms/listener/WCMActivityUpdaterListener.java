eb63ec030fa531dce653cd2f9121db3691dd488c
tu-vu-duy <tuvd@exoplatform.com>
2015-07-09 08:37:03 +0700

    SOC-4954 | [Social Activity Addon] Implements the Listener on INTEG to update the activityId

--- integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/addons/rdbms/listener/WCMActivityUpdaterListener.java
+++ integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/addons/rdbms/listener/WCMActivityUpdaterListener.java
@@ -1,10 +1,16 @@
 package org.exoplatform.wcm.addons.rdbms.listener;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.exoplatform.commons.utils.ActivityTypeUtils;
 import org.exoplatform.services.listener.Event;
 import org.exoplatform.services.listener.Listener;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.wcm.core.NodeLocation;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.plugin.doc.UIDocActivity;
 import org.exoplatform.wcm.ext.component.activity.UILinkActivity;
 import org.exoplatform.wcm.ext.component.activity.listener.Utils;
 
@@ -17,7 +23,8 @@ public class WCMActivityUpdaterListener extends Listener<ExoSocialActivity, Stri
   @Override
   public void onEvent(Event<ExoSocialActivity, String> event) throws Exception {
     ExoSocialActivity oldActivity = event.getSource();
-    switch (oldActivity.getType()) {
+    String type = (oldActivity.getType() == null) ? "" : oldActivity.getType();
+    switch (type) {
     case UILinkActivity.ACTIVITY_TYPE:
       migrationLinkActivity(oldActivity, event.getData());
       break;
@@ -33,27 +40,63 @@ public class WCMActivityUpdaterListener extends Listener<ExoSocialActivity, Stri
   }
 
   private void migrationLinkActivity(ExoSocialActivity oldActivity, String newId) {
-    if (oldActivity.isComment()) {
-      LOG.info(String.format("Migration the %s link comment '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
-    } else {
-      LOG.info(String.format("Migration the %s link activity '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
-    }
   }
 
   private void migrationContentSpaceActivity(ExoSocialActivity oldActivity, String newId) {
-    if (oldActivity.isComment()) {
-      LOG.info(String.format("Migration the %s ContentSpace comment '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
+  }
+
+  private void migrationFileSpaceActivity(ExoSocialActivity activity, String newId) throws RepositoryException {
+    if (activity.isComment()) {
+      // TODO: Needs to confirm with ECMS team about the comment type
+      // Utils.CONTENT_SPACES = "contents:spaces" Asks ECMS team to update the comment
+      // There is new mixin type define to keep the CommentId
+      // private static String MIX_COMMENT = "exo:activityComment";
+      // private static String MIX_COMMENT_ID = "exo:activityCommentID";
+      LOG.info(String.format("Migration file-spaces comment '%s' with new id's %s", activity.getTitle(), newId));
+      //
+      migrationDoc(activity, newId);
     } else {
-      LOG.info(String.format("Migration the %s ContentSpace activity '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
+      LOG.info(String.format("Migration file-spaces activity '%s' with new id's %s", activity.getTitle(), newId));
+      //
+      migrationDoc(activity, newId);
     }
   }
 
-  private void migrationFileSpaceActivity(ExoSocialActivity oldActivity, String newId) {
-    if (oldActivity.isComment()) {
-      LOG.info(String.format("Migration the %s FileSpace comment '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
-    } else {
-      LOG.info(String.format("Migration the %s FileSpace activity '%s' with old id's %s and new id's %s", oldActivity.getType(), oldActivity.getTitle(), oldActivity.getId(), newId));
+  private void migrationDoc(ExoSocialActivity activity, String newId) throws RepositoryException {
+    String workspace = activity.getTemplateParams().get(UIDocActivity.WORKSPACE);
+    if (workspace == null) {
+      workspace = activity.getTemplateParams().get(UIDocActivity.WORKSPACE.toLowerCase());
+    }
+    String docId = activity.getTemplateParams().get(UIDocActivity.ID);
+    String path = activity.getUrl();
+    LOG.info("Migration doc: " + workspace + " " + docId + " " + path);
+    Node docNode = getDocNode(workspace, path, docId);
+    if (docNode != null && docNode.isNodeType(ActivityTypeUtils.EXO_ACTIVITY_INFO)) {
+      LOG.info("Migration doc: " + docNode.getPath());
+      try {
+        ActivityTypeUtils.attachActivityId(docNode, newId);
+        docNode.save();
+      } catch (RepositoryException e) {
+        LOG.warn("Updates the file-spaces activity is unsuccessful!");
+        LOG.debug("Updates the file-spaces activity is unsuccessful!", e);
+      }
     }
   }
 
+  /**
+   * This method is target to get the Document node.
+   * 
+   * @param workspace
+   * @param path
+   * @param nodeId
+   * @return
+   */
+  private Node getDocNode(String workspace, String path, String nodeId) {
+    NodeLocation nodeLocation = new NodeLocation();
+    nodeLocation.setSystemSession(true);
+    nodeLocation.setUUID(nodeId);
+    nodeLocation.setPath(path);
+    //
+    return NodeLocation.getNodeByLocation(nodeLocation);
+  }
 }
