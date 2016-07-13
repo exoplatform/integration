c4853187ced9d170bc734c49e272329e513ba0ad
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-07-13 13:53:05 +0200

    PLF-6708 : Update REST service to retrieve rendered document

--- integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/ext/component/document/service/ContentViewerRESTService.java
+++ integ-ecms/integ-ecms-social/src/main/java/org/exoplatform/wcm/ext/component/document/service/ContentViewerRESTService.java
@@ -18,10 +18,13 @@ package org.exoplatform.wcm.ext.component.document.service;
 
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Locale;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.portlet.ActionRequest;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,10 +35,17 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.ecm.resolver.JCRResourceResolver;
+import org.exoplatform.groovyscript.text.BindingContext;
 import org.exoplatform.portal.application.PortalApplication;
 import org.exoplatform.portal.application.PortalRequestContext;
+import org.exoplatform.portal.webui.util.Util;
 import org.exoplatform.portal.webui.workspace.UIPortalApplication;
+import org.exoplatform.resolver.ResourceResolver;
+import org.exoplatform.services.cms.templates.TemplateService;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.app.SessionProviderService;
@@ -44,6 +54,7 @@ import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.resources.Orientation;
 import org.exoplatform.services.rest.resource.ResourceContainer;
+import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.services.wcm.utils.WCMCoreUtils;
 import org.exoplatform.social.plugin.doc.UIDocViewer;
 import org.exoplatform.web.ControllerContext;
@@ -51,7 +62,10 @@ import org.exoplatform.web.WebAppController;
 import org.exoplatform.web.application.Application;
 import org.exoplatform.webui.application.WebuiApplication;
 import org.exoplatform.webui.application.WebuiRequestContext;
+import org.exoplatform.webui.core.UIComponent;
+import org.exoplatform.webui.core.lifecycle.WebuiBindingContext;
 import org.gatein.api.site.SiteType;
+import org.gatein.pc.portlet.impl.jsr168.api.ActionRequestImpl;
 
 /**
  *
@@ -61,9 +75,12 @@ public class ContentViewerRESTService implements ResourceContainer {
 
   private static final Log LOG = ExoLogger.getLogger(ContentViewerRESTService.class.getName());
 
+  private WebAppController webAppController;
+
   private RepositoryService repositoryService;
 
-  public ContentViewerRESTService(RepositoryService repositoryService) throws Exception {
+  public ContentViewerRESTService(WebAppController webAppController, RepositoryService repositoryService) throws Exception {
+    this.webAppController = webAppController;
     this.repositoryService = repositoryService;
   }
 
@@ -99,10 +116,8 @@ public class ContentViewerRESTService implements ResourceContainer {
       uiDocViewer.setOriginalNode(contentNode);
       uiDocViewer.setNode(contentNode);
 
-      WebAppController controller = (WebAppController) ExoContainerContext.getCurrentContainer().getComponentInstanceOfType(WebAppController.class);
-
-      ControllerContext controllerContext = new ControllerContext(controller, controller.getRouter(), request, response, null);
-      PortalApplication application = controller.getApplication(PortalApplication.PORTAL_APPLICATION_ID);
+      ControllerContext controllerContext = new ControllerContext(webAppController, webAppController.getRouter(), request, response, null);
+      PortalApplication application = webAppController.getApplication(PortalApplication.PORTAL_APPLICATION_ID);
       PortalRequestContext requestContext = new PortalRequestContext(application, controllerContext, org.exoplatform.portal.mop.SiteType.PORTAL.toString(), "", "", null);
       WebuiRequestContext.setCurrentInstance(requestContext);
       requestContext.setUIApplication(new UIPortalApplication());
@@ -113,10 +128,9 @@ public class ContentViewerRESTService implements ResourceContainer {
       content = writer.toString();
 
     } catch (Exception e) {
-      if (LOG.isErrorEnabled()) {
-        LOG.error(e);
-      }
+      LOG.error("Cannot render content of document " + repoName + "/" + workspaceName + "/" + uuid, e);
     }
+
     return Response.ok(content).build();
   }
 
@@ -125,14 +139,41 @@ public class ContentViewerRESTService implements ResourceContainer {
     return service.getSystemSessionProvider(null);
   }
 
-  public class MyPortalRequestContext extends PortalRequestContext {
-    public MyPortalRequestContext(WebuiApplication app, ControllerContext controllerContext, String requestSiteType, String requestSiteName, String requestPath, Locale requestLocale) {
-      super(app, controllerContext, requestSiteType, requestSiteName, requestPath, requestLocale);
+  public String getTemplate(Node contentNode) {
+    if(contentNode == null) {
+      return null;
+    }
+    TemplateService templateService = CommonsUtils.getService(TemplateService.class);
+    String userName = ConversationState.getCurrent().getIdentity().getUserId();
+    try {
+      String nodeType = contentNode.getPrimaryNodeType().getName();
+      if(templateService.isManagedNodeType(nodeType)) {
+        return templateService.getTemplatePathByUser(false, nodeType, userName);
+      }
+    }catch (RepositoryException re){
+      if (LOG.isDebugEnabled() || LOG.isWarnEnabled())
+        LOG.error("Get template catch RepositoryException: ", re);
+    }
+    catch (Exception e) {
+      LOG.warn(e.getMessage(), e);
     }
 
-    @Override
-    public Orientation getOrientation() {
-      return Orientation.LT;
+    return null;
+  }
+
+  public void processRender(String template, UIComponent uiComponent,  Writer writer) throws Exception {
+    if(template == null) {
+      throw new IllegalStateException();
+    } else {
+      ExoContainer container = ExoContainerContext.getCurrentContainer();
+      ResourceResolver resolver = new JCRResourceResolver("dms-system");
+      BindingContext bcontext = new BindingContext(resolver, writer);
+      bcontext.put("_ctx", bcontext);
+      bcontext.put("uicomponent", uiComponent);
+      bcontext.put(uiComponent.getUIComponentName(), uiComponent);
+      bcontext.put("locale", new Locale("en"));
+      org.exoplatform.groovyscript.text.TemplateService service = container.getComponentInstanceOfType(org.exoplatform.groovyscript.text.TemplateService.class);
+      service.merge(template, bcontext);
     }
   }
 
